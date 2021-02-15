72c889ab58a1050ad9cacb967c4cd2b469a757b2
unknown <lavellthedev+github@gmail.com>
2021-02-15 01:02:30 -0500

    spinning cursor on deck

--- js/app.js
+++ js/app.js
@@ -2,12 +2,33 @@ const degreeToRadian = (angle) => {
   return angle * (Math.PI / 180);
 };
 
-const radius = 80;
+const radius = 50;
 const diameter = radius * 2;
 
-const circle = document.querySelector(".cursor");
-circle.style.width =  `${diameter}px`;
-circle.style.height=  `${diameter}px`;
+const circle = document.querySelector("#circle");
+circle.style.width = `${diameter}px`;
+circle.style.height = `${diameter}px`;
+
+const text = circle.dataset.text;
+const characters = text.split("");
+
+const deltaAngle = 360 / characters.length;
+const characterOffsetAngle = 10;
+let currentAngle = -90;
+
+characters.forEach((character, index) => {
+  const span = document.createElement("span");
+  span.innerText = character;
+  const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+  const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+  const transform = `translate(${xPos}px, ${yPos}px)`;
+  const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+  span.style.transform = `${transform} ${rotate}`;
+
+  currentAngle += deltaAngle;
+  circle.appendChild(span);
+});
 
 
 let mouseCursor = document.querySelector('.cursor');
