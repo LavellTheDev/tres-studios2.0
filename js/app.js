97a16180a91be73b995fdc7529aa034284aa75ba
unknown <lavellthedev+github@gmail.com>
2021-02-14 22:42:20 -0500

    tres studios spinning cursor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+const degreeToRadian = (angle) => {
+  return angle * (Math.PI / 180);
+};
+
+const radius = 80;
+const diameter = radius * 2;
+
+const circle = document.querySelector(".cursor");
+circle.style.width =  `${diameter}px`;
+circle.style.height=  `${diameter}px`;
+
+
 let mouseCursor = document.querySelector('.cursor');
 let navLinks = document.querySelectorAll('.main');
+
 
 window.addEventListener('mousemove', cursor);
 
