0f83b3a9aed7d9f2db362bf25bc1191da0a74b74
LavellTheDev <lavellthedev+github@gmail.com>
2021-09-03 11:35:26 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
-const degreeToRadian = (angle) => {
-  return angle * (Math.PI / 180);
-};
-
-const radius = 70;
-const diameter = radius * 2;
-
-const circle = document.querySelector("#circle");
-circle.style.width = `${diameter}px`;
-circle.style.height = `${diameter}px`;
-
-const text = circle.dataset.text;
-const characters = text.split("");
-
-const deltaAngle = 360 / characters.length;
-const characterOffsetAngle = 8;
-let currentAngle = -90;
-
-characters.forEach((character, index) => {
-  const span = document.createElement("span");
-  span.innerText = character;
-  const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
-  const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
-
-  const transform = `translate(${xPos}px, ${yPos}px)`;
-  const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
-  span.style.transform = `${transform} ${rotate}`;
-
-  currentAngle += deltaAngle;
-  circle.appendChild(span);
-});
-
-
-let mouseCursor = document.querySelector('.cursor');
-let navLinks = document.querySelectorAll('.main');
-
-
-window.addEventListener('mousemove', cursor);
-
-function cursor(e){
-  mouseCursor.style.top = e.pageY + "px";
-  mouseCursor.style.left = e.pageX + "px"
-}
+// const degreeToRadian = (angle) => {
+//   return angle * (Math.PI / 180);
+// };
+//
+// const radius = 70;
+// const diameter = radius * 2;
+//
+// const circle = document.querySelector("#circle");
+// circle.style.width = `${diameter}px`;
+// circle.style.height = `${diameter}px`;
+//
+// const text = circle.dataset.text;
+// const characters = text.split("");
+//
+// const deltaAngle = 360 / characters.length;
+// const characterOffsetAngle = 8;
+// let currentAngle = -90;
+//
+// characters.forEach((character, index) => {
+//   const span = document.createElement("span");
+//   span.innerText = character;
+//   const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+//   const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+//
+//   const transform = `translate(${xPos}px, ${yPos}px)`;
+//   const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+//   span.style.transform = `${transform} ${rotate}`;
+//
+//   currentAngle += deltaAngle;
+//   circle.appendChild(span);
+// });
+//
+//
+// let mouseCursor = document.querySelector('.cursor');
+// let navLinks = document.querySelectorAll('.main');
+//
+//
+// window.addEventListener('mousemove', cursor);
+//
+// function cursor(e){
+//   mouseCursor.style.top = e.pageY + "px";
+//   mouseCursor.style.left = e.pageX + "px"
+// }
